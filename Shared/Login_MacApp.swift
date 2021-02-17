d0b0539c5a3ace5686010586419cca320f3c7d30
Elechim01 <mikymanny015@gmail.com>
2021-02-17 22:40:05 +0100

    Initial Commit

---
+++ Shared/Login_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login_MacApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 17/02/21.
+//
+
+import SwiftUI
+
+@main
+struct Login_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
